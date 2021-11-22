d281dd49ae966f529fe34c57616b5565a7019a33
Alexandr Siryj <theobroma222@gmail.com>
2021-11-22 17:27:10 +0200

    connect to rapidapi

--- src/@api/api.ts
+++ src/@api/api.ts
@@ -1,13 +1,14 @@
 // https://stackoverflow.com/questions/54496398/typescript-type-string-undefined-is-not-assignable-to-type-string
-// https://stackoverflow.com/questions/43014034/setting-authorization-header-in-axios
 import axios from 'axios';
 
 export const API_KEY = process.env.REACT_APP_API_KEY as string;
 export const API_URL = process.env.REACT_APP_API_URL as string;
+export const API_HOST = process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST as string;
 
 export const instance = axios.create({
   baseURL: API_URL,
   headers: {
-    'x-access-token': API_KEY,
+    'x-rapidapi-key': API_KEY,
+    'x-rapidapi-host': API_HOST,
   },
 });
