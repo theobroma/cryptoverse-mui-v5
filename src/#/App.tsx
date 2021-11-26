891000acfa6fb87ab74d581cc270594712bb5126
Alexandr Siryj <theobroma222@gmail.com>
2021-11-26 20:44:16 +0200

    add router

--- src/#/App.tsx
+++ src/#/App.tsx
@@ -1,12 +1,30 @@
 // https://fettblog.eu/react-types-for-children-are-broken/
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainView from '../@views/MainView';
 import { GuestLayout } from './Layouts';
 
 export const AppContainer = () => {
   return (
-    <GuestLayout>
-      <MainView />
-    </GuestLayout>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<GuestLayout>Home</GuestLayout>} />
+        <Route
+          path="exchanges"
+          element={<GuestLayout>Exchanges</GuestLayout>}
+        />
+        <Route
+          path="cryptocurrencies"
+          element={
+            <GuestLayout>
+              <MainView />
+            </GuestLayout>
+          }
+        />
+      </Routes>
+      {/* <GuestLayout>
+        <MainView />
+      </GuestLayout> */}
+    </BrowserRouter>
   );
 };
