b4af4f16e4cc43df4dab0b7b02e1b865f49d2e99
Alexandr Siryj <theobroma222@gmail.com>
2021-12-05 22:21:52 +0200

    html-react-parser

--- src/@views/CryptoDetailsView/CryptoDetailsView.tsx
+++ src/@views/CryptoDetailsView/CryptoDetailsView.tsx
@@ -1,5 +1,7 @@
 import { Container, Grid, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
+// import HTMLReactParser from 'html-react-parser';
+import parse from 'html-react-parser';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useGetCryptoDetailsQuery } from '../../@store/coins/crypto/cryptoApi';
@@ -7,16 +9,24 @@ import { useGetCryptoDetailsQuery } from '../../@store/coins/crypto/cryptoApi';
 const CryptoDetailsView: React.FC = () => {
   const { id } = useParams();
   const { data, isFetching } = useGetCryptoDetailsQuery(id);
-  const cryptoDetails = data?.data?.coin || '';
+  const cryptoDetails = data?.data?.coin;
 
   return (
     <Container maxWidth="lg">
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
-          <Grid item xs={12} md={6} lg={4}>
-            <Typography component="h2" variant="h5">
-              {cryptoDetails.name} ({cryptoDetails.slug}) Price
-            </Typography>
+          <Grid item xs={12}>
+            {cryptoDetails && (
+              <>
+                <Typography component="h2" variant="h5">
+                  {cryptoDetails.name} ({cryptoDetails.slug}) Price
+                </Typography>
+                <Typography component="h2" variant="h5">
+                  What is {cryptoDetails.name}?
+                </Typography>
+                {parse(cryptoDetails.description)}
+              </>
+            )}
           </Grid>
         </Grid>
       </Box>
