1a7160a9e1043840d36a9166348f56b6f37b5aa6
Alexandr Siryj <theobroma222@gmail.com>
2021-12-06 02:34:11 +0200

    crypto details links

--- src/@views/CryptoDetailsView/CryptoDetailsView.tsx
+++ src/@views/CryptoDetailsView/CryptoDetailsView.tsx
@@ -1,4 +1,4 @@
-import { Container, Grid, Typography } from '@mui/material';
+import { Container, Grid, Typography, Link as MuiLink } from '@mui/material';
 import Box from '@mui/material/Box';
 // import HTMLReactParser from 'html-react-parser';
 import parse from 'html-react-parser';
@@ -22,10 +22,37 @@ const CryptoDetailsView: React.FC = () => {
             <Typography component="h2" variant="h5">
               {cryptoDetails.name} ({cryptoDetails.slug}) Price
             </Typography>
+            <img
+              style={{ width: '35px', height: '35px' }}
+              src={cryptoDetails.iconUrl}
+              alt={cryptoDetails.name}
+            />
             <Typography component="h2" variant="h5">
               What is {cryptoDetails.name}?
             </Typography>
             {parse(cryptoDetails.description)}
+          </Grid>
+
+          <Grid item xs={12}>
+            <Typography component="h3" variant="h6">
+              {cryptoDetails.name} Links
+            </Typography>
+            <Box sx={{ typography: 'body1', ml: 2 }}>
+              {cryptoDetails.links?.map((link: any) => (
+                <>
+                  <span style={{ marginRight: '5px' }}>{link.type}</span>
+                  <MuiLink
+                    href={link.url}
+                    underline="hover"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {link.name}
+                  </MuiLink>
+                  <br />
+                </>
+              ))}
+            </Box>
           </Grid>
         </Grid>
       </Box>
