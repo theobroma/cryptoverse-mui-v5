34d9ce9fcd5c4cdbb56aca9b76ce9a620c0cd108
Alexandr Siryj <theobroma222@gmail.com>
2022-09-16 06:28:24 +0300

    HideOnScroll appbar

--- src/@components/AppBar/Drawer.tsx
+++ src/@components/AppBar/Drawer.tsx
@@ -13,6 +13,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
 import BackToTop from '../UI/BackToTop';
+import HideOnScroll from '../UI/HideOnScroll';
 
 import { AppBar, DrawerHeader, drawerWidth, Main } from './Drawer.styled';
 import NestedList from './NestedList';
@@ -31,30 +32,32 @@ const PersistentDrawerLeft = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <Box sx={{ display: 'flex' }} className="HolyGrail-content">
-      <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <RouterLink
-            to={{ pathname: '/' }}
-            style={{
-              color: 'white',
-              textDecoration: 'none',
-            }}
-          >
-            <Typography variant="h6" noWrap component="div">
-              Cryptoverse
-            </Typography>
-          </RouterLink>
-        </Toolbar>
-      </AppBar>
+      <HideOnScroll>
+        <AppBar position="fixed" open={open}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{ mr: 2, ...(open && { display: 'none' }) }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <RouterLink
+              to={{ pathname: '/' }}
+              style={{
+                color: 'white',
+                textDecoration: 'none',
+              }}
+            >
+              <Typography variant="h6" noWrap component="div">
+                Cryptoverse
+              </Typography>
+            </RouterLink>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
       <BackToTop />
       <Drawer
         sx={{
